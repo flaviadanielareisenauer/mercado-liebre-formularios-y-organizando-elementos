be8f72ae8cbe43148917a535511cc6dfbd80afb0
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2021-12-11 19:47:26 -0300

    agregando rutas de login y registro

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+
 app.listen(port, () => {
   console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`);
 });
